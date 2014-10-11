dac74da6d23c80caee8ffdea45bb897ca85b1515
thorhildurmagnusdottir <thorhildurmagnudottir@gmail.com>
2014-10-11 12:52:43 +0000

    Skoða SAX parser fyrir issue #11

--- RestaurantApp/app/src/main/java/com/gunnarsturla/restaurantappgi/Communicator.java
+++ RestaurantApp/app/src/main/java/com/gunnarsturla/restaurantappgi/Communicator.java
@@ -1,5 +1,9 @@
 package com.gunnarsturla.restaurantappgi;
 
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
 /**
  * Created by thorhildur on 10.10.2014.
  * This class communicates with the web service to update the xml file if
@@ -7,6 +11,17 @@ package com.gunnarsturla.restaurantappgi;
  */
 public class Communicator {
     public int versionNumber;
+    public String webUrl;
+    public XMLReader webreader; //SAX Xml parser
+
+    public Communicator() {
+        try {
+            webreader = XMLReaderFactory.createXMLReader(); //xml parser
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+        webUrl = "file:///home/thorhildur/AndroidStudioProjects/RestaurantApp/dummy.xml"; //web service url
+    }
 
     public int getVersionNumber() {
         return versionNumber;
