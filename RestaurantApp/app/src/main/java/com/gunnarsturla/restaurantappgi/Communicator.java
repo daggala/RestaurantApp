d341cdf46550aaabc999ef4b83c79732451ff5c7
thorhildurmagnusdottir <thorhildurmagnudottir@gmail.com>
2014-10-11 10:52:14 +0000

    Git commit-push test android studio thorhildur

--- RestaurantApp/app/src/main/java/com/gunnarsturla/restaurantappgi/Communicator.java
+++ RestaurantApp/app/src/main/java/com/gunnarsturla/restaurantappgi/Communicator.java
@@ -4,6 +4,7 @@ package com.gunnarsturla.restaurantappgi;
  * Created by thorhildur on 10.10.2014.
  * This class communicates with the web service to update the xml file if
  * a new version is available.
+ * Add a new comment for git test
  */
 public class Communicator {
     public int versionNumber;
