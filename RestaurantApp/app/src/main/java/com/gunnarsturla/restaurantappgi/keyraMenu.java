4d4fc07086f3f2941b48dc0ddd63a5f3a7d96954
daggala <daggala@gmail.com>
2014-10-15 10:12:20 +0000

    Keyrslufæll fyrir submenus

--- RestaurantApp/app/src/main/java/com/gunnarsturla/restaurantappgi/keyraMenu.java
+++ RestaurantApp/app/src/main/java/com/gunnarsturla/restaurantappgi/keyraMenu.java
@@ -5,6 +5,19 @@ package com.gunnarsturla.restaurantappgi;
  */
 public class keyraMenu {
 
-    SubMenu.
+    public static void main (String[] args) {
+
+
+
+        Item item1 = new Item(1, 999, 520, "Horpuskeljar", "Gott", "Rjomi, sjavardyr og allskonar", "https://www.google.is/images/srpr/logo11w.png");
+        Item item2 = new Item(2, 1999, 920, "Hamborgari", "Snilld", "Kal, gurka, tomatur, kjot", "https://childadolescentweightmanagement.files.wordpress.com/2012/07/juicy-chicken-burger-pic-19-hamburger-ideas4.jpgg");
+        Item item3 = new Item(item2);
+        SubMenu[] listSubMenus = new SubMenu[200];
+
+
+
+        System.out.println("....");
+
+    }
 
 }
